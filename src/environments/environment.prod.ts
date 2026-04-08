cf00218d936fb17162bae3d460eace2726e3dade
Joel <starcrinea@gmail.com>
2026-04-08 11:56:54 -0500

    update redirectUri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
 
   api: {
-    baseUrl: 'https://tu-api.azurewebsites.net/api'
+    baseUrl: 'https://fns-maziapp-backend-qa-hbe6eya5arfna9ey.centralus-01.azurewebsites.net/api'
   },
 
   azure: {
     clientId: 'd5d696b8-5856-4d96-90d0-249eb10d20fe',
     tenantId: 'e0ccfa01-ef7c-4ce5-ac1c-b7eea9b006cb',
-    redirectUri: 'https://tu-frontend.azurewebsites.net',
+    redirectUri: 'https://lively-plant-04d3ad710.7.azurestaticapps.net',
 
     scopes: {
       api: 'api://d5d696b8-5856-4d96-90d0-249eb10d20fe/access_as_user',
