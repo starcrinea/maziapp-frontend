62630e804e2a1a71767e09f45cb6fac48a7a5ea8
starcrinea <starcrinea@gmail.com>
2026-05-12 20:11:18 -0500

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
 
   api: {
-    baseUrl: 'https://fns-maziapp-backend-qa-hbe6eya5arfna9ey.centralus-01.azurewebsites.net/api'
+    baseUrl: 'https://fns-maziapp-backend-qa-hbe6eya5arfna9ey.centralus-01.azurewebsites.net/api',
   },
 
   azure: {
@@ -11,8 +11,8 @@ export const environment = {
     redirectUri: 'https://lively-plant-04d3ad710.7.azurestaticapps.net',
 
     scopes: {
-      api: 'api://d5d696b8-5856-4d96-90d0-249eb10d20fe/access_as_user',
-      graph: 'User.Read'
-    }
-  }
+      api: 'api://73ace681-92a9-4062-846a-99e6104a772a/access_as_user',
+      graph: 'User.Read',
+    },
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
   api: {
-    baseUrl: 'http://localhost:7071/api'
+    baseUrl: 'http://localhost:7071/api',
   },
 
   azure: {
@@ -11,8 +11,8 @@ export const environment = {
     redirectUri: 'http://localhost:4200',
 
     scopes: {
-      api: 'api://d5d696b8-5856-4d96-90d0-249eb10d20fe/access_as_user',
-      graph: 'User.Read'
-    }
-  }
+      api: 'api://73ace681-92a9-4062-846a-99e6104a772a/access_as_user',
+      graph: 'User.Read',
+    },
+  },
 };
